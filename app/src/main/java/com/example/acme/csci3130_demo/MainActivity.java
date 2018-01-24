d0a5acb1e3f75d80b2be83fec6d951beb28afc9c
Samblam <sam.barefoot@dal.ca>
2018-01-23 21:57:33 -0400

    Added Functionality so that A user can input text, and then press a button that copies it to a text field located center screen

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.acme.csci3130_demo;
 
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText Input = findViewById(R.id.Input);
+        final Button button = findViewById(R.id.Copy);
+        final TextView hello = findViewById(R.id.hello);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hello.setText(Input.getText());
+            }
+        });
     }
+
+
 }
