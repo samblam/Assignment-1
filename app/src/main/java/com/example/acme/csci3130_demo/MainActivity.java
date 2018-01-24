741b5ac81016bdf1b298f1aea81a71f7d44d595d
Samblam <sam.barefoot@dal.ca>
2018-01-23 21:58:02 -0400

    Added Functionality so that A user can input text, and then press a button that copies it to a text field located center screen

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 hello.setText(Input.getText());
+                
             }
         });
     }
